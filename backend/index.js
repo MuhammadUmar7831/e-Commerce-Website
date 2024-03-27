26568189d7bcec50a720614f504f47fd5ec78fda
Muhammad Umar <mu8494759@gmail.com>
2024-03-27 16:20:27 +0500

    product.js added

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,14 @@
 const connectToDatabase = require('./database');
+const express = require("express");
+const app = express();
+const cors = require("cors");
 
-// Connect to the database
+// Middleware
+app.use(cors());
+app.use(express.json()); 
 const connection = connectToDatabase();
-connection.end();
+
+const port = 3000; // Choose any available port you prefer
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
