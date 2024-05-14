3323a06122c57abe12e98cf6664acbef564a6c8d
Muhammad Umar <mu8494759@gmail.com>
2024-05-14 21:04:13 +0500

    mysql database

--- backend/database.js
+++ backend/database.js
@@ -1,21 +1,20 @@
-const Sql = require('mysql2');
+const mysql = require('mysql2');
 
 function connectToDatabase() {
-  // create a new Sql connection
-  const connection = Sql.createConnection({
-    // host: 'Sql-e3a7385-mu8494759-744f.a.aivencloud.com',
-    host: 'DESKTOP-OJOIMTP',
+  // create a new MySQL connection
+  const connection = mysql.createConnection({
+    host: 'mysql-e3a7385-mu8494759-744f.a.aivencloud.com',
     port: 27674,
-    user: 'DESKTOP-OJOIMTP\mu849',
-    // password: 'AVNS_0txvLOBA7WGArsaXV7U',
+    user: 'avnadmin',
+    password: 'AVNS_0txvLOBA7WGArsaXV7U',
     database: 'defaultdb'
   });
 
   connection.connect((error) => {
     if (error) {
-      console.error('Error connecting to Sql database:', error);
+      console.error('Error connecting to MySQL database:', error);
     } else {
-      console.log('Connected to Sql database!');
+      console.log('Connected to MySQL database!');
     }
   });
 
